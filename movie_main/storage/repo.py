630cb898738b4666bc43696e4fef36d547be8fea
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2025-10-12 02:57:03 +0200

    Uppdaterad FavoritesManager i repo.py

--- movie_main/storage/repo.py
+++ movie_main/storage/repo.py
@@ -1,73 +1,60 @@
 import json
 
-# En klass som behållare för all funktionalitet kring favoriter.
-# Tre metoder: spara, läsa och radera favoriter-listan i JSON.
-
 class FavoritesManager:
     def __init__(self, filename):
         self.filename = filename
 
-    def save_favorites(self, movie):                        # Spara en film
-        try:
-                                                        # Om filen finns, läs vad som finns i den
+    def save_favorites(self, movie):   
+
+        try:                                           
             with open(self.filename, "r", encoding="utf-8") as f:
                 favorites = json.load(f)
         except (json.JSONDecodeError, FileNotFoundError):
-                                                            # Om filen saknas eller är tom/ogiltig, skapa tom lista
+                                                
             favorites = []
 
-                                            # Kolla så att filmen inte redan finns i listan
         if not any(f["title"] == movie.title for f in favorites):
-            favorites.append(movie.to_dict())  # Lägg filmen i listan
-
+            favorites.append(movie.to_dict())  
 
             with open(self.filename, "w", encoding="utf-8") as f:
-                json.dump(favorites, f, indent=4, ensure_ascii=False) # Spara den nya listan i JSON-filen
+                json.dump(favorites, f, indent=4, ensure_ascii=False) 
 
-            # Returnera bekräftelse till UI
             return {"success": True, "message": f"Filmen '{movie.title}' har sparats."}
         else:
             return {"success": False, "message": f"Filmen '{movie.title}' finns redan i favoriter."}
 
-    # Funktion för att ta fram en lista med filmer ur JSON
-
     def get_favorites(self):
+
         try:
             with open(self.filename, "r", encoding="utf-8") as f:
                 favorites = json.load(f)
-
-                # Returnera listan om det finns något i den, annars en tom lista
                 return favorites if favorites else []
             
         except (json.JSONDecodeError, FileNotFoundError):
             return []
 
-    # Funktion för att radera en film ur JSON
-
-    def remove_favorite(self, index_to_remove):  #index to remove användarens val från UI
+    def remove_favorite(self, index_to_remove):
        
-        favorites = self.get_favorites()    # Hämtar listan med filmer
-        if not favorites:                   # Om listan är tom
-            return []
-
-        # Kolla att användarens val finns i listans intervall
+        favorites = self.get_favorites() 
+        if not favorites:                   
+            return {"success": False, "message": "Listan är tom."}
+        
+        index_to_remove -= 1
 
         if 0 <= index_to_remove < len(favorites):
-            removed_movie = favorites.pop(index_to_remove)  # skapar variabel med borttagna filmen
+            removed_movie = favorites.pop(index_to_remove)  
         with open(self.filename, "w", encoding="utf-8") as f:
-        # Spara uppdaterad lista utan vald film tillbaka till JSON
             json.dump(favorites, f, indent=4, ensure_ascii=False)
 
-            return {"success": True, "Removed_movie": removed_movie}  # Returnera filmen som togs bort
+            return {"success": True, "Removed_movie": removed_movie}
         else:
             return {"success": False, "message": "Ogiltigt val. Ingen film raderades."}
 
-
-    # Funktion för att radera alla favoriter i JSON
     def clear_favorites(self):
+
         with open(self.filename, "w", encoding="utf-8") as f:
-            favorites = []  # Skapa tom lista
-            json.dump(favorites, f, indent=4, ensure_ascii=False)  # Skriv tom lista till filen
+            favorites = []  
+            json.dump(favorites, f, indent=4, ensure_ascii=False)  
         return {"success": True, "message": "Alla favoriter har raderats."}
 
     
